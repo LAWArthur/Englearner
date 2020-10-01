2f4b576f2aedb0ec1ba2c427acb85df8ad7f1289
LAWArthur <jincai3335_micro@163.com>
2020-10-01 17:21:36 +0800

    upd

--- exercises/translation/scripts/index.js
+++ exercises/translation/scripts/index.js
@@ -1,8 +1,9 @@
-let showFirst,loadFormer;
+let showFirst,loadFormer,oneTranslation;
 let vocabulary;
 const vocabularies={
     "mid_vocab": "中考考纲词汇/词组"
 };
+let current;
 
 ($(()=>{
     getVocabularies();
@@ -10,10 +11,12 @@ const vocabularies={
     $("#start").click(()=>{
         showFirst = $("#showfirst").prop("checked");
         loadFormer = $("#loadformer").prop("checked");
+        oneTranslation = $("#onetranslation").prop("checked");
         console.log(showFirst,loadFormer);
         $.get(`../../data/vocabulary/${$("#vocabulary").val()}.json`,(data,status) => {
             if(status=="success"){
-                console.log(data,status);
+                vocabulary = data;
+                initializeExercise();
             }
             else console.log(`Error ${status}`);
         });
@@ -29,4 +32,17 @@ function getVocabularies(){
     for(let i in vocabularies){
         sel.append($("<option></option>").val(i).text(vocabularies[i]));
     }
+}
+
+function initializeExercise(){
+    $(".vocabname").text(vocabulary.name);
+    generate();
+}
+
+function generate(){
+    current = vocabulary.vocabulary[Math.floor(Math.random()*vocabulary.vocabulary.length)];
+    let meaning = current.meanings[Math.floor(Math.random()*current.meanings.length)];
+    let translation = meaning.translations[Math.floor(Math.random()*meaning.translations.length)];
+    $(".question").text(`${meaning.type}. ${onetranslation?translation:meaning.translations.join("；")}`);
+
 }
