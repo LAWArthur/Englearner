b52956db6d70140b23c74da836eb1fa659d3b604
LAWArthur <jincai3335_micro@163.com>
2020-10-01 16:05:03 +0800

    iw

---
+++ exercises/translation/scripts/index.js
@@ -0,0 +1,32 @@
+let showFirst,loadFormer;
+let vocabulary;
+const vocabularies={
+    "mid_vocab": "中考考纲词汇/词组"
+};
+
+($(()=>{
+    getVocabularies();
+
+    $("#start").click(()=>{
+        showFirst = $("#showfirst").prop("checked");
+        loadFormer = $("#loadformer").prop("checked");
+        console.log(showFirst,loadFormer);
+        $.get(`../../data/vocabulary/${$("vocabulary").val()}.json`,(data,status) => {
+            if(status>=200&&status<400){
+                console.log(data,status);
+            }
+            else console.log(`Error ${status}`);
+        });
+
+        
+    })
+}))();
+
+
+function getVocabularies(){
+    let sel = $("#vocabulary");
+
+    for(let i in vocabularies){
+        sel.append($("<option></option>").val(i).text(vocabularies[i]));
+    }
+}
